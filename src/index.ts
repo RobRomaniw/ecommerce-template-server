3cebbdd3fe547f094b262db8b8baad93adba337b
Rob Romaniw <robert.romaniw@argogroupus.com>
2021-05-25 15:57:20 -0400

    setup graphQL server with type-graphql schema

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,33 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entitites/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express'; 
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+
+const __PORT__ = '4000';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-  const post = orm.em.create(Post, { title: "my first post" });
-  await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log('[posts]', posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  })
+
+  apolloServer.applyMiddleware({ app });
+  
+  app.listen(__PORT__, () => {
+    console.log(`server started on localhost:${__PORT__}`)
+  })
 };
 
 main().catch((err) => {
-  console.error("[DB ERROR:] " + err.message);
+  console.error("[MAIN ERROR:] " + err.message);
 });
