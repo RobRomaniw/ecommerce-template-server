a59eb5b7dc58f649ef9f1437c7ab62e9c784bd67
Rob Romaniw <robert.romaniw@argogroupus.com>
2021-05-27 19:12:02 -0400

    user resolver updates

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import redis from "redis";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { MyContext } from "./types";
+import cors from 'cors';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -21,6 +22,13 @@ const main = async () => {
 
   let RedisStore = connectRedis(session);
   let redisClient = redis.createClient();
+
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true
+    })
+  )
 
   app.use(
     session({
@@ -49,7 +57,10 @@ const main = async () => {
     context: ({req, res}): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ 
+    app,
+    cors: false // setting at server level above
+  });
 
   app.listen(CONSTANTS.__PORT__, () => {
     console.log(`server started on localhost:${CONSTANTS.__PORT__}`);
